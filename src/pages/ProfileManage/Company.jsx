50570d5d9bf0152f613493de6a1f32a4b4230e1f
Ngọc Huy Nguyễn <huynguyen.160303hl@gmail.com>
2024-10-17 00:02:52 +0700

    Xong xem ds ung vien va xem chi tiet ung vien

--- src/pages/ProfileManage/Company.jsx
+++ src/pages/ProfileManage/Company.jsx
@@ -1,207 +1,232 @@
-import React from "react";
-import { Formik, Form, Field } from "formik";
-import { TextField } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Formik, Form } from "formik";
+import InputField from "../../components/Auth/InputField";
+import TextAreaField from "../../components/Auth/TextAreaField";
 import AuthButton from "../../components/Auth/AuthButton";
-import {
-  Business as BusinessIcon,
-  Phone as PhoneIcon,
-  Language as WebsiteIcon,
-  DateRange as DateIcon,
-  PhotoCamera as CameraIcon,
-} from "@mui/icons-material";
-import IconButton from "@mui/material/IconButton";
-import {
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  InputAdornment,
-} from "@mui/material";
+import { Business as BusinessIcon, Phone as PhoneIcon, Language as WebsiteIcon, DateRange as DateIcon, PhotoCamera as CameraIcon } from "@mui/icons-material";
+import IconButton from '@mui/material/IconButton';
+import Cookies from "js-cookie";
 
-const Company = ({ onClose }) => {
-  const initialValues = {
-    name: "",
-    description: "",
-    hotline: "",
-    website: "",
-    founded_year: "",
-    avatar: null,
-    avatarPreview: "",
+const Company = () => {
+  const [avatarUser, setAvatarUser] = useState(null);
+  const [dataProfile, setDataProfile] = useState({});
+
+  const accessToken = Cookies.get("access_token");
+
+  useEffect(() => {
+    const apiURL = process.env.REACT_APP_API_URL + "/company/profile/";
+  
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiURL, {
+          method: 'GET',
+          headers: {
+            Authorization: 'Bearer ' + accessToken,
+          },
+        });
+  
+        if (res.status === 200) {
+          const data = await res.json();
+          setDataProfile(data || {});
+          setAvatarUser(data.avatar ? data.avatar : '');
+          console.log(">>> Data: ", data);
+        } else {
+          console.log("Không thể lấy dữ liệu hồ sơ");
+        }
+      } catch (error) {
+        console.log("Lỗi khi lấy dữ liệu:", error);
+      }
+    };
+  
+    fetchData();
+  }, [accessToken]);
+
+  const handleSubmit = async (values) => {
+    const dataProfile = {
+      name: values.name,
+      description: values.description,
+      hotline: values.hotline,
+      website: values.website,
+      founded_year: values.founded_year
+    };
+  
+    const apiURL = process.env.REACT_APP_API_URL + "/company/profile/";
+  
+    try {
+      const res = await fetch(apiURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + accessToken,
+        },
+        body: JSON.stringify(dataProfile),
+      });
+  
+      if (res.ok) {
+        alert('Thông tin được cập nhật thành công');
+      } else {
+        alert('Cập nhật thông tin thất bại');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    console.log("Company Form Values:", values);
-    setSubmitting(false);
-    onClose && onClose();
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    const apiURL = process.env.REACT_APP_API_URL + "/company/upload-avatar/";
+    if (file && (file.type === "image/jpeg" || file.type === "image/png")) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setAvatarUser(reader.result);
+        };
+        reader.readAsDataURL(file);
+
+        try {
+          const formData = new FormData();
+          formData.append('avatar', file);
+      
+          const response = await fetch(apiURL, {
+            method: 'POST',
+            headers: {
+              Authorization: 'Bearer ' + accessToken,
+            },
+            body: formData,
+          });
+      
+          if (response.ok) {
+            const result = await response.json();
+            console.log('Upload successful:', result);
+          } else {
+            throw new Error(`Error: ${response.statusText}`);
+          }
+        } catch(error) {
+          console.log(error);
+        }
+    } else {
+        alert('Your image format is invalid. Please select another one.');
+    }
   };
 
   return (
-    <>
-      <DialogTitle>Edit Company Profile</DialogTitle>
-      <DialogContent>
-        <div className="max-w-4xl mx-auto p-4">
-          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            {({ errors, touched, setFieldValue, values, isSubmitting }) => (
-              <Form>
-                {/* Avatar Upload Section */}
-                <div className="relative mb-8 flex justify-center">
-                  <div className="relative w-32 h-32">
-                    {values.avatarPreview ? (
-                      <img
-                        src={values.avatarPreview}
-                        alt="Avatar Preview"
-                        className="w-full h-full object-cover rounded-full"
-                      />
-                    ) : (
-                      <div className="w-full h-full bg-gray-200 rounded-full flex items-center justify-center">
-                        <BusinessIcon style={{ fontSize: 64, color: "gray" }} />
-                      </div>
-                    )}
+    <div>
+      <div className="max-w-4xl mx-auto p-4">
+        <h2 className="text-2xl font-bold text-center mb-8">Company Information</h2>
 
-                    <IconButton
-                      component="label"
-                      className="absolute bottom-0 right-0 bg-gray-800 p-1 rounded-full text-white"
-                      style={{ transform: "translate(160%, -90%)" }}
-                    >
-                      <span className="flex items-center justify-center w-9 h-9 border-2 border-white rounded-full bg-black bg-opacity-60">
-                        <CameraIcon
-                          style={{ fontSize: "20px", color: "white" }}
-                        />
-                      </span>
-                      <input
-                        type="file"
-                        accept="image/*"
-                        onChange={(event) => {
-                          const file = event.currentTarget.files[0];
-                          setFieldValue("avatar", file);
-                          if (file) {
-                            const reader = new FileReader();
-                            reader.onloadend = () => {
-                              setFieldValue("avatarPreview", reader.result);
-                            };
-                            reader.readAsDataURL(file);
-                          }
-                        }}
-                        hidden
-                      />
-                    </IconButton>
-                  </div>
+        <Formik 
+          initialValues={{
+            name: dataProfile.name || '',
+            description: dataProfile.description || '',
+            hotline: dataProfile.hotline || '',
+            website: dataProfile.website || '',
+            founded_year: dataProfile.founded_year || '',
+          }} 
+          onSubmit={handleSubmit}
+          enableReinitialize
+        >
+          {({ errors, touched, setFieldValue, values }) => (
+            <Form>
+              {/* Avatar Upload Section */}
+              <div className="relative mb-8 flex justify-center">
+                <div className="relative w-32 h-32">
+                  {avatarUser ? (
+                    <img 
+                      src={avatarUser} 
+                      alt="Avatar Preview" 
+                      className="w-full h-full object-cover rounded-full" 
+                    />
+                  ) : (
+                    <div className="w-full h-full bg-gray-200 rounded-full flex items-center justify-center">
+                      <BusinessIcon style={{ fontSize: 64, color: 'gray' }} />
+                    </div>
+                  )}
+
+                  <IconButton
+                    component="label"
+                    className="absolute bottom-0 right-0 bg-gray-800 p-1 rounded-full text-white"
+                    style={{ transform: 'translate(160%, -90%)' }}
+                  >
+                    <span className="flex items-center justify-center w-9 h-9 border-2 border-white rounded-full bg-black bg-opacity-60">
+                      <CameraIcon style={{ fontSize: '20px', color: 'white' }} />
+                    </span>
+                    <input
+                      type="file"
+                      accept='.jpg, .jpeg, .png'
+                      onChange={handleImageChange}
+                      hidden
+                    />
+                  </IconButton>
                 </div>
+              </div>
 
-                {/* Form Grid Layout */}
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                  <Field
-                    as={TextField}
-                    label="Company Name"
-                    name="name"
-                    type="text"
-                    placeholder="Enter company name"
-                    fullWidth
-                    variant="outlined"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <BusinessIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                    error={touched.name && errors.name}
-                    helperText={touched.name && errors.name}
-                  />
+              {/* Form Grid Layout */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <InputField
+                  label="Company Name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter company name"
+                  icon={BusinessIcon}
+                  errors={errors}
+                  touched={touched}
+                />
 
-                  <Field
-                    as={TextField}
-                    label="Hotline"
-                    name="hotline"
-                    type="tel"
-                    placeholder="Enter hotline number"
-                    fullWidth
-                    variant="outlined"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <PhoneIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                    error={touched.hotline && errors.hotline}
-                    helperText={touched.hotline && errors.hotline}
-                  />
+                <InputField
+                  label="Hotline"
+                  name="hotline"
+                  type="tel"
+                  placeholder="Enter hotline number"
+                  icon={PhoneIcon}
+                  errors={errors}
+                  touched={touched}
+                />
 
-                  <Field
-                    as={TextField}
-                    label="Website"
-                    name="website"
-                    type="url"
-                    placeholder="Enter company website"
-                    fullWidth
-                    variant="outlined"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <WebsiteIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                    error={touched.website && errors.website}
-                    helperText={touched.website && errors.website}
-                  />
+                <InputField
+                  label="Website"
+                  name="website"
+                  type="url"
+                  placeholder="Enter company website"
+                  icon={WebsiteIcon}
+                  errors={errors}
+                  touched={touched}
+                />
 
-                  <Field
-                    as={TextField}
-                    label="Founded Year"
-                    name="founded_year"
-                    type="date"
-                    placeholder="Enter founded year"
-                    fullWidth
-                    variant="outlined"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <DateIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                    error={touched.founded_year && errors.founded_year}
-                    helperText={touched.founded_year && errors.founded_year}
-                  />
-                </div>
+                <InputField
+                  label="Founded Year"
+                  name="founded_year"
+                  type="date"
+                  placeholder="Enter founded year"
+                  icon={DateIcon}
+                  errors={errors}
+                  touched={touched}
+                />
+              </div>
 
-                {/* Description TextField with Full Width */}
-                <div className="my-8">
-                  <Field
-                    as={TextField}
-                    label="Description"
-                    name="description"
-                    placeholder="Enter company description"
-                    fullWidth
-                    variant="outlined"
-                    multiline
-                    rows={6}
-                    error={touched.description && errors.description}
-                    helperText={touched.description && errors.description}
-                  />
-                </div>
+              {/* Description TextArea with Full Width */}
+              <div className="my-8">
+                <TextAreaField
+                  label="Description"
+                  name="description"
+                  placeholder="Enter company description"
+                  icon={BusinessIcon}
+                  rows={6}
+                  style={{ height: '150px' }}
+                  errors={errors}
+                  touched={touched}
+                />
+              </div>
 
-                {/* Save Information Button */}
-                <DialogActions>
-                  <Button onClick={onClose}>Cancel</Button>
-                  <AuthButton
-                    label="Save Information"
-                    type="submit"
-                    className="w-auto"
-                    disabled={isSubmitting}
-                  />
-                </DialogActions>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      </DialogContent>
-    </>
+              {/* Save Information Button */}
+              <AuthButton
+                label="Save Information"
+                type="submit"
+                className="w-auto"
+              />
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Recruiter/ManageJobs.jsx
+++ src/pages/Recruiter/ManageJobs.jsx
@@ -13,7 +13,10 @@ import EditJob from "../../components/Recruiter/EditJob";
 import EditIcon from "@mui/icons-material/Edit";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import Cookies from "js-cookie";
-
+import { style } from "framer-motion/client";
+import { motion } from "framer-motion";
+import KeyboardArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardArrowLeftOutlined";
+import ApplicationFile from "../../components/Recruiter/ApplicationFile";
 const ManageJobs = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -21,6 +24,10 @@ const ManageJobs = () => {
   const [selectedJob, setSelectedJob] = useState(null);
   const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
   const [status, setStatus] = useState(null);
+  const [showCandidate, setShowCandidate] = useState(false);
+  const [showDetail, setShowDetail] = useState(false);
+  const [candidates, setCandidates] = useState([]);
+  const [dataDetail, setDataDetail] = useState({});
 
   useEffect(() => {
     fetchJobs();
@@ -37,12 +44,17 @@ const ManageJobs = () => {
           Authorization: `Bearer ${accessToken}`,
         },
       });
-
+      console.log(">>> ", res);
       if (res.ok) {
         const data = await res.json();
+
         setJobs(data);
       } else {
-        setStatus({ error: "Failed to fetch jobs. Please try again." });
+        const errorData = await res.json();
+        console.log('Error response from server:', errorData);
+        setStatus({
+          error: errorData.detail || "Failed to fetch jobs. Please try again.",
+        });
       }
     } catch (error) {
       setStatus({ error: "Network error. Please check your connection." });
@@ -130,110 +142,209 @@ const ManageJobs = () => {
     }
   };
 
+  const handleShowList = (id) => {
+    setShowCandidate(!showCandidate);
+    fetchCandidates(id);
+  };
+
+  const handleOpenShowDetail = (data) => {
+    setShowDetail(true);
+    setDataDetail(data);
+  }
+
+  // -----------------
+  const fetchCandidates = async (jobId) => {
+    const apiURL = `${process.env.REACT_APP_API_URL}/job/get_list_candidate_applied_for_job/?job_id=${jobId}`;
+    const accessToken = Cookies.get("access_token");
+
+    try {
+      const res = await fetch(apiURL, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setCandidates(data);
+      } else {
+        console.log("Error");
+      }
+    } catch (error) {
+      setStatus({ error: "Network error. Please check your connection." });
+    }
+  };
+  // -----------------
+
   return (
-    <Box m="20px">
-      <Header title="MANAGE JOBS" subtitle="View and Manage Job Listings" />
+    <>
+      <Box m="20px" style={{ display: showCandidate ? 'none' : 'block' }}>
+        <Header title="MANAGE JOBS" subtitle="View and Manage Job Listings" />
 
-      {status && (
-        <Box mb="20px">
-          <Typography
-            color={
-              status.success ? colors.greenAccent[500] : colors.redAccent[500]
-            }
-          >
-            {status.success || status.error}
-          </Typography>
-        </Box>
-      )}
-
-      <Box mt="40px">
-        <Typography
-          variant="h4"
-          color={colors.grey[100]}
-          fontWeight="bold"
-          mb="15px"
-        >
-          Current Job Listings
-        </Typography>
-        <Box
-          display="grid"
-          gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-          gap="20px"
-        >
-          {jobs.map((job) => (
-            <Box
-              key={job.id}
-              backgroundColor={colors.primary[400]}
-              borderRadius="4px"
-              p="15px"
+        {status && (
+          <Box mb="20px">
+            <Typography
+              color={
+                status.success ? colors.greenAccent[500] : colors.redAccent[500]
+              }
             >
-              <Typography
-                variant="h5"
-                color={colors.grey[100]}
-                fontWeight="bold"
+              {status.success || status.error}
+            </Typography>
+          </Box>
+        )}
+
+        <Box mt="40px">
+          <Typography
+            variant="h4"
+            color={colors.grey[100]}
+            fontWeight="bold"
+            mb="15px"
+          >
+            Current Job Listings
+          </Typography>
+          <Box
+            display="grid"
+            gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+            gap="20px"
+          >
+            {jobs.map((job) => (
+              <Box
+                key={job.id}
+                backgroundColor={colors.primary[400]}
+                borderRadius="4px"
+                p="15px"
               >
-                {job.title}
-              </Typography>
-              <Typography variant="body2" color={colors.grey[300]}>
-                Category: {job.job_category.title}
-              </Typography>
-              <Typography variant="body2" color={colors.grey[300]}>
-                Location: {job.location}
-              </Typography>
-              <Typography variant="body2" color={colors.grey[300]}>
-                Salary Range: {job.salary_range}
-              </Typography>
-              <Typography variant="body2" color={colors.grey[300]}>
-                Experience: {job.experience}
-              </Typography>
-              <Box mt="10px">
-                <Chip label={job.level} size="small" color="default" />
-                <Chip
-                  label={job.status ? "Active" : "Inactive"}
-                  size="small"
-                  color={job.status ? "info" : "error"}
-                  sx={{ ml: 1 }}
-                />
-              </Box>
-              <Tooltip title={job.description}>
                 <Typography
-                  variant="body2"
-                  color={colors.grey[300]}
-                  sx={{
-                    mt: 1,
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    display: "-webkit-box",
-                    WebkitLineClamp: 2,
-                    WebkitBoxOrient: "vertical",
-                  }}
+                  variant="h5"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
                 >
-                  {job.description}
+                  {job.title}
                 </Typography>
-              </Tooltip>
-              <Box mt="10px" display="flex" justifyContent="flex-end">
-                <IconButton onClick={() => handleEdit(job)} color="secondary">
-                  <EditIcon />
-                </IconButton>
-                <IconButton onClick={() => handleHide(job.id)} color="warning">
-                  <VisibilityOffIcon />
-                </IconButton>
+                <Typography variant="body2" color={colors.grey[300]}>
+                  Category: {job.job_category.title}
+                </Typography>
+                <Typography variant="body2" color={colors.grey[300]}>
+                  Location: {job.location}
+                </Typography>
+                <Typography variant="body2" color={colors.grey[300]}>
+                  Salary Range: {job.salary_range}
+                </Typography>
+                <Typography variant="body2" color={colors.grey[300]}>
+                  Experience: {job.experience}
+                </Typography>
+                <Box mt="10px">
+                  <Chip label={job.level} size="small" color="default" />
+                  <Chip
+                    label={job.status ? "Active" : "Inactive"}
+                    size="small"
+                    color={job.status ? "info" : "error"}
+                    sx={{ ml: 1 }}
+                  />
+                </Box>
+                <Tooltip title={job.description}>
+                  <Typography
+                    variant="body2"
+                    color={colors.grey[300]}
+                    sx={{
+                      mt: 1,
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      display: "-webkit-box",
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: "vertical",
+                    }}
+                  >
+                    {job.description}
+                  </Typography>
+                </Tooltip>
+                <div style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center'
+                }}>
+                  <motion.button
+                    className=" text-blueColor hover:text-[#535ac8] font-semibold transition duration-200"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    onClick={() => handleShowList(job.id)}
+                  >
+                    <span>Show</span>
+                  </motion.button>
+
+                  <Box mt="10px" display="flex" justifyContent="">
+                    <IconButton onClick={() => handleEdit(job)} color="secondary">
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton onClick={() => handleHide(job.id)} color="warning">
+                      <VisibilityOffIcon />
+                    </IconButton>
+                  </Box>
+                </div>
               </Box>
-            </Box>
-          ))}
+            ))}
+          </Box>
         </Box>
+
+        <EditJob
+          open={isEditDialogOpen}
+          onClose={() => {
+            setIsEditDialogOpen(false);
+            setSelectedJob(null);
+          }}
+          onSave={handleUpdatedJob}
+          job={selectedJob}
+        />
       </Box>
 
-      <EditJob
-        open={isEditDialogOpen}
-        onClose={() => {
-          setIsEditDialogOpen(false);
-          setSelectedJob(null);
-        }}
-        onSave={handleUpdatedJob}
-        job={selectedJob}
-      />
-    </Box>
+      <Box m="20px" style={{ display: showCandidate ? 'block' : 'none' }}>
+        <motion.button
+          onClick={handleShowList}
+          className="mb-6 flex items-center text-blueColor hover:text-[#535ac8] font-semibold transition duration-200"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          <KeyboardArrowLeftOutlinedIcon className="mr-1" />
+          <span>Back</span>
+        </motion.button>
+
+        {candidates.map((item) => (
+          <Box
+            key={item.id}
+            backgroundColor={colors.primary[400]}
+            borderRadius="4px"
+            p="15px"
+          >
+            <Typography variant="h5" color={colors.grey[100]}>
+              {item.candidate.full_name}
+            </Typography>
+            <Typography variant="body2" color={colors.grey[300]}>
+              Email: {item.candidate.email}
+            </Typography>
+            <Typography variant="body2" color={colors.grey[300]}>
+              Phone: {item.candidate.phone_number}
+            </Typography>
+            <motion.button
+              className="mt-2 text-blueColor hover:text-[#535ac8] font-semibold transition duration-200"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={() => handleOpenShowDetail(item)}
+            >
+              Detail
+            </motion.button>
+          </Box>
+        ))} 
+
+        <ApplicationFile 
+          open={showDetail}
+          onClose={() => {
+            setShowDetail(false);
+          }}
+          dataDetail={dataDetail}
+        />
+      </Box>
+    </>
   );
 };
 
